9c70299edd90e2543d98f99a1a70edd99ff703f8
KavinPratap <kavin.pratap@gmail.com>
2025-10-28 18:25:44 +0000

    pause

--- FINAL_PROJECT.py
+++ FINAL_PROJECT.py
@@ -1,49 +1,87 @@
-import math
-import time
-import random
+import math #imports the math library
+import time #imports the time library
+import random #imports the random library
 
-Characteristics = ["THE RIZZLER","Kindness","greedy","The SIGMA","Anger Issues","STUPID","DEPRESSION","CHAD","GREAT CHAD","SUPER guy","Happy","Joyfull","SIXSEVENnator","none"]
+#the traits of the character
+Characteristics = ["THE RIZZLER","Kindness","greedy","The SIGMA","Anger Issues","STUPID","DEPRESSION","CHAD","GREAT CHAD","SUPER guy","Happy","Joyfull","SIXSEVENnator","none","VERY CHARMING","VERY SPECIAL"]
+
 AmountOfNPCS = 0
+#Variable declaration
+
+#Enter a while loop
 while 1:
+    #get the amount of npcs the user wants
     AmountOfNPCS = int(input("Input amount of npcs: "))
-    if AmountOfNPCS > 0 and AmountOfNPCS < 101:
+    #if statement to make sure user does not enter negative or to high numbers
+    if AmountOfNPCS > 0 and AmountOfNPCS < 31:
         break
+        #breaks condition if true
 
-age = 0
+age = 0; count = 0
+#Variable declaration
 
-count = 0
 
-print(isAnimal)
+#Enters the primary while loop to print npcs
 while count < AmountOfNPCS:
     print("\nNPC", count+1)
+    #prints the npc the loop is on
     
     randChar = random.choice(Characteristics)
+    #gets a random trait
 
     print("Characteristic: ", randChar)
+    #prints the random trait from earlier on line 29
 
     age = random.randint(1,101)
+    #gets a random number for age
 
     print("Age: ", age)
+    #prints the age
 
-
+    #Infinite while loop to make sure the float is not to small
     while 1:
+        #gets a random float
         floatation = random.random()*10
+        #checks if the float is between the boundries
         if floatation > 0 and floatation < 11:
             break
+            #breaks if true
 
-    if randChar == "Anger Issues":
-        print("Anger Issue trigger(tollerance, higher equal worse): ",round(floatation, 1))
-
+    #Checks if the randchar variable is not "none" from the list
+    if randChar != "none":
+        #prints an intensity of that trait in the character
+        print("Intensity of trait(10 is intense): ",round(floatation, 1))
+    
+    #same thing as line 42
     while 1:
         floatation = random.random()*10
         if floatation > 2 and floatation < 7:
             break
-    
-    print("The Height(feet): ", round(floatation, 1))
 
-    isAnimal = random.randint(0,2)
+    #prints the height of the character
+    print(f"The height of them is   {round(floatation, 1)}   feet/foot", )
+
+    #makes a random integer from 0 to 10
+    isAnimal = random.randint(0,11)
+
+    #if statement to change the probability from 50/50 to 30/70
+    if isAnimal < 4:
+        isAnimal = 1
+        #sets is animal to true
+    else:
+        isAnimal = 0
+        #sets is animal to false
+
     print("Is an animal: ", bool(isAnimal))
+    #prints if the character will be an animal, it also typecasts the variable to a boolean
+
+    print()
+    #new line
 
     count += 1
+    #increment count by 1
+    
+    time.sleep(0.2)
+    #adds a slight delay to the loop
 
 
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FINAL-PROJECT
 PROJECT
+
+This code file is to make npcs with random traits. It first gets the user input, giving the user a chance to input a number between 1 to 30. This is to stop to many from being generated and crashing the program.
